45d8d7495e31f008a9aff4687205896d8ad07aed
hansmalvin <hansmalvin7@gmail.com>
2024-11-23 16:39:08 +0700

    new changes

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -127,6 +127,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
                 child: const BarGraph(),
               ),
+              // diapus nig bawahnya
               const SizedBox(height: 20),
               Container(
                 margin: const EdgeInsets.only(left: 16, right: 16, bottom: 16),
@@ -188,30 +189,73 @@ class _BarGraphState extends State<BarGraph> {
   }
 }
 
-// class Test extends StatefulWidget {
-//   const Test({super.key});
+class HomeTest extends StatefulWidget {
+  const HomeTest({super.key});
 
-//   @override
-//   State<Test> createState() => _TestState();
-// }
+  @override
+  State<HomeTest> createState() => _HomeTestState();
+}
 
-// class _TestState extends State<Test> {
-//   final user = FirebaseAuth.instance.currentUser;
-
-//   signout() async {
-//     await FirebaseAuth.instance.signOut();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Center(
-//         child: Text('${user!.email}'),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: (() => signout()),
-//         child: const Icon(Icons.login_rounded),
-//       ),
-//     );
-//   }
-// }
+class _HomeTestState extends State<HomeTest> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            leading: IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.menu_sharp),
+            ),
+            backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+            elevation: 0,
+            pinned: true,
+            centerTitle: false,
+            expandedHeight: 300.0,
+            stretch: true,
+            flexibleSpace: const FlexibleSpaceBar(
+              stretchModes: [
+                StretchMode.zoomBackground,
+              ],
+              background: Image(
+                image: AssetImage(
+                    'https://images.unsplash.com/photo-1416339306562-f3d12fefd36f?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&s=92f3e02f63678acc8416d044e189f515'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SliverAppBar(
+            backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+            elevation: 0,
+            pinned: true,
+            bottom: const PreferredSize(
+                preferredSize: Size.fromHeight(-10.0), child: SizedBox()),
+            flexibleSpace: const CarouselHome(),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Padding(
+                  padding:
+                      const EdgeInsets.only(left: 8, bottom: 20, right: 10),
+                  child: Container(
+                    margin: const EdgeInsets.only(left: 6, right: 6, top: 10),
+                    padding: const EdgeInsets.all(18),
+                    decoration: BoxDecoration(
+                      color: Constants.colorBlue3,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    // height: 200,
+                    // width: MediaQuery.of(context).size.width,
+                    child: const BarGraph(),
+                  ),
+                );
+              },
+              // childCount: 20,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
